cec7f983ac02b981a02686cf432482d857156a5c
Braden <fish2355@hotmail.com>
2019-12-03 10:49:46 -0700

    No idea where new files came from, Old file before modifying

---
+++ img/React App_files/main.710cc4d9a3ae5d9d6ed4.hot-update.js
@@ -0,0 +1,108 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/App.js":
+/*!********************!*\
+  !*** ./src/App.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _App_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.css */ "./src/App.css");
+/* harmony import */ var _App_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_App_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_DisplayComponents_Display__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/DisplayComponents/Display */ "./src/components/DisplayComponents/Display.js");
+/* harmony import */ var _components_DisplayComponents_Logo__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/DisplayComponents/Logo */ "./src/components/DisplayComponents/Logo.js");
+/* harmony import */ var _components_ButtonComponents_NumberButtons_Numbers__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/ButtonComponents/NumberButtons/Numbers */ "./src/components/ButtonComponents/NumberButtons/Numbers.js");
+/* harmony import */ var _components_ButtonComponents_OperatorButtons_Operators__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./components/ButtonComponents/OperatorButtons/Operators */ "./src/components/ButtonComponents/OperatorButtons/Operators.js");
+/* harmony import */ var _components_ButtonComponents_SpecialButtons_Specials__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/ButtonComponents/SpecialButtons/Specials */ "./src/components/ButtonComponents/SpecialButtons/Specials.js");
+var _jsxFileName = "/Users/Braden/Documents/Lambda_FSW/2. Web Applications I/2. Intro to React/2. Props/lambda-calculator/src/App.js";
+
+ // import { numbers, operators, specials } from "./data";
+
+ // STEP 4 - import the button and display components
+// Don't forget to import any extra css/scss files you build into the correct component
+
+
+
+
+
+
+function App() {
+  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
+  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
+  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
+  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
+  // Don't forget to pass the functions (and any additional data needed) to the components as props
+  // const [numberState, setNumberState] = useState(numbers);
+  // const [operatorsState, setOperatorsState] = useState(operators);
+  // const [specialsState, setSpecialsState] = useState(specials);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_DisplayComponents_Logo__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "App",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_DisplayComponents_Display__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "functButtons",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inputs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ButtonComponents_SpecialButtons_Specials__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ButtonComponents_NumberButtons_Numbers__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_ButtonComponents_OperatorButtons_Operators__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }))));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (App);
+
+/***/ })
+
+})
+//# sourceMappingURL=main.710cc4d9a3ae5d9d6ed4.hot-update.js.map
